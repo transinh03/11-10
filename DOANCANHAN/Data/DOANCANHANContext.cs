a3ae247d8bf8cb6b6cb00b42d4a1e7e7af9c51e8
transinh03 <trancongthiensinh030072003@example.com>
2025-10-11 12:03:44 +0700

    init project

---
+++ DOANCANHAN/Data/DOANCANHANContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using API.Models;
+
+namespace DOANCANHAN.Data
+{
+    public class DOANCANHANContext : DbContext
+    {
+        public DOANCANHANContext (DbContextOptions<DOANCANHANContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<API.Models.Voucher> Voucher { get; set; } = default!;
+    }
+}
